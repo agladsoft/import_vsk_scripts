a1429c0df6223c18f9692057d392765cd8687f28
Timur <timurka.zavyalov@mail.ru>
2022-08-26 15:21:18 +0300

    git init

---
+++ bash_dir/run_import_vsk.sh
@@ -0,0 +1,5 @@
+while true;
+do
+	${XL_IDP_ROOT_VSK_IMPORT}/bash_dir/flat_import_vsk.sh;
+	sleep 1;
+done
---
+++ scripts_for_bash_with_inheritance/flat_import_vsk.py
@@ -0,0 +1,71 @@
+import datetime
+import json
+import os
+import sys
+import contextlib
+import numpy as np
+import pandas as pd
+
+input_file_path = os.path.abspath(sys.argv[1])
+output_folder = sys.argv[2]
+
+headers_eng = {
+    "Год": "year",
+    "Месяц": "month",
+    "Период": "period",
+    "Линия": "line",
+    "Порт": "departure_port",
+    "Страна": "departure_country",
+    "Отправитель": "shipper_name",
+    "Получатель": "consignee_name",
+    "Экспедитор": "expeditor",
+    "Груз": "goods_name",
+    "Тип контейнера": "container_type",
+    "Размер контейнера": "container_size",
+    "Кол-во контейнеров, шт.": "container_count",
+    "Терминал": "terminal",
+    "TEU": "teu",
+    "Номер контейнера": "container_number",
+    "КОД ТНВЭД": "tnved",
+    "Группа груза по ТНВЭД": "tnved_group_id",
+    "Наименование Группы": "tnved_group_name",
+    "ИНН": "shipper_inn",
+    "УНИ-компания": "shipper_name_unified",
+    "Страна КОМПАНИИ": "shipper_country",
+    "Направление": "direction",
+    "Тип убытия": "departure_type",
+    "Коносамент": "consignment",
+    "Судно": "ship_name",
+    "Рейс": "voyage",
+    "Тип парка": "park_type",
+    "Агент": "agent",
+    "Станция УКП": "station_ukp",
+    "Сборный груз": "combined_cargo",
+    "Станция назначени (план)": "destination_station",
+    "Вес нетто": "goods_weight_netto",
+    "Вес брутто": "goods_weight_brutto"
+}
+
+
+df = pd.read_csv(input_file_path)
+df = df.replace({np.nan: None})
+df = df.rename(columns=headers_eng)
+df = df.loc[:, ~df.columns.isin(['direction', 'tnved_group_name', 'shipper_inn',
+                                 'shipper_name_unified', 'departure_country'])]
+parsed_data = df.to_dict('records')
+for dict_data in parsed_data:
+    for key, value in dict_data.items():
+        with contextlib.suppress(Exception):
+            if key in ['year', 'month', 'teu', 'container_size']:
+                dict_data[key] = int(value)
+            elif key in ['tnved_group_id']:
+                dict_data[key] = f"{int(value):02d}"
+            elif key == 'terminal':
+                dict_data[key] = os.environ.get('XL_VSK_IMPORT')
+
+    dict_data['original_file_name'] = os.path.basename(input_file_path)
+    dict_data['original_file_parsed_on'] = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+basename = os.path.basename(input_file_path)
+output_file_path = os.path.join(output_folder, f'{basename}.json')
+with open(f"{output_file_path}", 'w', encoding='utf-8') as f:
+    json.dump(parsed_data, f, ensure_ascii=False, indent=4)
