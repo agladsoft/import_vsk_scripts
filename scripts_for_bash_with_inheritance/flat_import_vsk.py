3e938d66133e816db1b25a8f6e7238436ef73ac0
timurzav <timurka.zavyalov@mail.ru>
2023-01-19 18:13:04 +0500

    add: removed NaN and empty rows

--- scripts_for_bash_with_inheritance/flat_import_vsk.py
+++ scripts_for_bash_with_inheritance/flat_import_vsk.py
@@ -87,7 +87,6 @@ for index, dict_data in enumerate(parsed_data):
         deleted_index.append(index)
 basename = os.path.basename(input_file_path)
 output_file_path = os.path.join(output_folder, f'{basename}.json')
-for index in deleted_index:
-    del parsed_data[index]
+parsed_data = [i for j, i in enumerate(parsed_data) if j not in deleted_index]
 with open(f"{output_file_path}", 'w', encoding='utf-8') as f:
     json.dump(parsed_data, f, ensure_ascii=False, indent=4)
