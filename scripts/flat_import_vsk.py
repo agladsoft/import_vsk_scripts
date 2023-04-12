acc9218e85becd0dcfaa64614ad60707bf86e144
timur <timurka.zavyalov@mail.ru>
2023-04-12 10:58:26 +0300

    git init

--- scripts/flat_import_vsk.py
+++ scripts/flat_import_vsk.py
@@ -53,13 +53,6 @@ class ImportVSK(object):
         self.output_folder: str = output_folder
 
     @staticmethod
-    def trim_all_columns(df) -> DataFrame:
-        """
-        Delete the empty lines at the beginning and at the end of the lines.
-        """
-        return df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
-
-    @staticmethod
     def change_type_and_values(df) -> None:
         """
         Change data types or changing values.
@@ -67,6 +60,13 @@ class ImportVSK(object):
         with contextlib.suppress(Exception):
             df['shipment_date'] = df['shipment_date'].dt.date.astype(str)
             df[['gtd_number']] = df[['gtd_number']].apply(lambda x: x.fillna('Нет данных'))
+
+    def add_new_columns(self, df) -> None:
+        """
+        Add new columns.
+        """
+        df['original_file_name'] = os.path.basename(self.input_file_path)
+        df['original_file_parsed_on'] = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
     def write_to_json(self, parsed_data) -> None:
         """
@@ -84,13 +84,12 @@ class ImportVSK(object):
         df: DataFrame = pd.read_excel(self.input_file_path)
         df = df.dropna(axis=0, how='all')
         df = df.rename(columns=headers_eng)
-        df['original_file_name'] = os.path.basename(self.input_file_path)
-        df['original_file_parsed_on'] = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         df = df.loc[:, ~df.columns.isin(['direction', 'tnved_group_name', 'shipper_inn',
                                          'shipper_name_unified', 'departure_country'])]
-        df = self.trim_all_columns(df)
-        df = df.replace({np.nan: None})
+        df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+        self.add_new_columns(df)
         self.change_type_and_values(df)
+        df = df.replace({np.nan: None})
         self.write_to_json(df.to_dict('records'))
 
 
