b5429c272e8d3a3505b32dc78f8c361cc7290b78
timur <timurka.zavyalov@mail.ru>
2023-09-07 15:57:23 +0300

    add: added new value (Импорт) in list

--- scripts/flat_import_vsk.py
+++ scripts/flat_import_vsk.py
@@ -86,7 +86,9 @@ class ImportVSK(object):
         original_columns = list(df.columns)
         df = df.rename(columns=headers_eng)
         renamed_columns = list(df.columns)
-        df = df.drop(columns=set(original_columns) & set(renamed_columns))
+        same_columns: set = set(original_columns) & set(renamed_columns)
+        if len(same_columns) != len(original_columns):
+            df = df.drop(columns=same_columns)
         df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
         self.add_new_columns(df)
         self.change_type_and_values(df)
